b28070676a5b85e2e9bfbbe60704cc55bc218140
Patcharapa520023 <614407039@parichat.skru.ac.th>
2021-05-14 00:05:50 +0700

    d

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
 <meta name="description" content="">
 <meta name="author" content="">	
 <!-- bootstrap css -->
+dsadasl,d,,,,,,dasasds
 <link href="css/app.css" rel="stylesheet" type="text/css" >
 <!-- style css -->
 <link rel="stylesheet" type="text/css" href="css/style.css">
