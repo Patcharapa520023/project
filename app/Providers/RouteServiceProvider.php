ac9796cea2d467501a571db1c53713a6345b2c10
Patcharapa520023 <614407039@parichat.skru.ac.th>
2021-05-20 01:04:34 +0700

    home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/หน้าแรก';
 
     /**
      * The controller namespace for the application.
