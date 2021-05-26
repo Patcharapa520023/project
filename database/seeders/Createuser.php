b35d5cffd3e37c97ccce7f0706a8fce6b329d873
Patcharapa520023 <614407039@parichat.skru.ac.th>
2021-05-26 21:41:00 +0700

    .

--- database/seeders/Createuser.php
+++ database/seeders/Createuser.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Department;
 class Createuser extends Seeder
 {
     /**
@@ -14,6 +15,12 @@ class Createuser extends Seeder
      */
     public function run()
     {
+        Department::create([
+        'dep_name'=>'กองการศึกษา',
+        'address'=>'เทศบาลสิงหนคร',
+        'telnum'=>'073274707',
+        'fax'=>'458788599'
+        ]);
         for ($i=1; $i <80 ; $i++) {
             $user=[
                 'email'=>"ft@gmaidl$i.com",
@@ -26,7 +33,8 @@ class Createuser extends Seeder
                 'lastname'=>'มาก'	,
                 'address'=>'45ซ.ยินดีเจริญจ.พัทลุง'	,
                 'telnum'=>'0891234881',
-                'position'=>'เจ้าหน้าที่ยาม'
+                'position'=>'เจ้าหน้าที่ยาม',
+                'department_id'=>'1'
             ];
             User::create($user)->personnel()->create($personnel);
         }
