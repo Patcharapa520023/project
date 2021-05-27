11a6b35f3d82734f516faa47ab25bbdd49b71e30
Anuchit Buaprom <com100pb@gmail.com>
2021-05-28 02:54:43 +0700

    d

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,9 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\JsonResponse;
 class LoginController extends Controller
 {
     /*
@@ -20,7 +19,6 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
     use AuthenticatesUsers;
 
     /**
@@ -39,6 +37,39 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+        $this->clearLoginAttempts($request);
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+        if(auth()->user()->roles=="admin"){
+            return $request->wantsJson()
+                        ? new JsonResponse([], 204)
+                        : redirect()->route('admin.dashboard');
+        }
+        return $request->wantsJson()
+                    ? new JsonResponse([], 204)
+                    : redirect()->route('user.home');
+    }
     public function logout(Request $request)
     {
         $this->guard()->logout();
@@ -53,6 +84,7 @@ class LoginController extends Controller
 
         return $request->wantsJson()
             ? new JsonResponse([], 204)
-            : redirect('/login');
+            : redirect()->route('login');
     }
+
 }
