6e60917ce55aaa75ee0164a4645cc5d27885fada
Mewadi11 <thisisthemay@gmail.com>
2021-09-01 22:57:41 +0700

    ทำตารางyear

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,8 @@ class Kernel extends HttpKernel
         'executive' => \App\Http\Middleware\Executive::class,
         'staff' => \App\Http\Middleware\Staff::class,
         'personnel' => \App\Http\Middleware\Personnel::class,
+        'year' => \App\Http\Middleware\Year::class,
+        
 
     ];
 }
